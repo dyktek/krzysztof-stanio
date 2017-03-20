43a797583d61d511e7fe691f374b72015dd00730
Katarzyna Fedevych <katarzynan@gmail.com>
2017-03-20 16:47:32 +0100

    comments seen in posts now, editor change

--- resources/views/chatter_comments.blade.php
+++ resources/views/chatter_comments.blade.php
@@ -2,15 +2,11 @@
         <li data-id="{{ $post->id }}" data-markdown="{{ $post->markdown }}">
             <b>Komentarz dodany przez {{ ucfirst($post->user->{Config::get('chatter.user.database_field_with_user_name')}) }}</b></a>
             {{ \Carbon\Carbon::createFromTimeStamp(strtotime($post->created_at))->diffForHumans() }}
-            @if($post->markdown)
-                <pre class="chatter_body_md">{{ $post->body }}</pre>
-                <?= \DevDojo\Chatter\Helpers\ChatterHelper::demoteHtmlHeaderTags(GrahamCampbell\Markdown\Facades\Markdown::convertToHtml($post->body)); ?>
-            <!--?= GrahamCampbell\Markdown\Facades\Markdown::convertToHtml( $post->body ); ?-->
-            @else
+            <div class="chatter_clear">
                 <?= $post->body; ?>
-            @endif
-            <div class="chatter_clear"></div>
+            </div>
         </li>
+    <hr>
         @endforeach
 <br>
 <h2 class="center indent">
