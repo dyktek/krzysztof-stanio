afe01d911c1c3b794b0a1eac88175d1a5dbe38de
Katarzyna Fedevych <katarzynan@gmail.com>
2017-03-25 14:19:24 +0100

    captcha

---
+++ config/recaptcha.php
@@ -0,0 +1,65 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | API Keys
+    |--------------------------------------------------------------------------
+    |
+    | Set the public and private API keys as provided by reCAPTCHA.
+    |
+    | In version 2 of reCAPTCHA, public_key is the Site key,
+    | and private_key is the Secret key.
+    |
+    */
+    'public_key'     => env('RECAPTCHA_PUBLIC_KEY', '6LcHABoUAAAAAFaJsyccXzLUd1jkFrkaJ6fraUBq'),
+    'private_key'    => env('RECAPTCHA_PRIVATE_KEY', '6LcHABoUAAAAAG9yFqd65b4Sg1jMfdhT548ua_a5'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Template
+    |--------------------------------------------------------------------------
+    |
+    | Set a template to use if you don't want to use the standard one.
+    |
+    */
+    'template'    => '',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Driver
+    |--------------------------------------------------------------------------
+    |
+    | Determine how to call out to get response; values are 'curl' or 'native'.
+    | Only applies to v2.
+    |
+    */
+    'driver'      => 'curl',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Options
+    |--------------------------------------------------------------------------
+    |
+    | Various options for the driver
+    |
+    */
+    'options'     => [
+
+        'curl_timeout' => 1,
+
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Version
+    |--------------------------------------------------------------------------
+    |
+    | Set which version of ReCaptcha to use.
+    |
+    */
+
+    'version'     => 2,
+
+];
