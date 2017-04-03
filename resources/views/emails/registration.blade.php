296ba8330bac7ac00db1710e41efded4d6a31f52
Katarzyna Fedevych <katarzynan@gmail.com>
2017-04-03 16:40:06 +0200

    registration mail

--- resources/views/emails/registration.blade.php
+++ resources/views/emails/registration.blade.php
@@ -1,16 +1,33 @@
 Dzień dobry!<br><br>
 
-Dziękujemy za zainteresowanie naszym kursem.
-Aby zarezerwować miejsce na kursie, wpłać opłatę rezerwacyjną.
-Dzięki wpłacie masz gwarancję
+Dziękujemy za zainteresowanie naszym kursem.<br>
+Najbliższa edycja zacznie się <b>25 maja 2017 r.</b> i będzie trwała 6 tygodni.<br>
+Podczas 157-godzinnego kursu zdobędziesz solidne podstawy, które pomogą Ci rozwijać karierę webdevelopera.
+Musisz jednak liczyć się z tym, że kurs to nie wszystko - czeka Cię dużo samodzielnej pracy, a kariera programisty
+to nauka, która się nie kończy :-)<br>
 
+Zajęcia prowadzone są w formie bootcampa - wraz z innymi uczestnikami przez 6 tygodni będziesz wykonywał różne zadania, zgodnie z programem
+zamieszczonym na <a href="/kursy-programowania" target="_blank">tej stronie</a>.
+Konieczne będzie przyniesienie własnego laptopa, który skonfigurujemy razem, na indywidualnym spotkaniu przed kursem.
+
+Wszystkie zajęcia prowadzone będą w Krakowie w naszym biurze przy ul. Ketlinga 1. W pobliżu można zaparkować samochodem, możliwy jest też dojazd komunikacją miejską
+liniami: 151, 178, 451. Budynek to domek zaadaptowany na biuro - przestronny, klimatyzowany. Do dyspozycji uczestników pozostaje wyposażona kuchnia i kilka łazienek.
+Osoby które są z daleka lub nie chcą tracić czasu na dojazd mogą bezpłatne nocować na górnych piętrach budynku, specjalnie do tego dostosowanych, co można zobaczyć na poniższym filmie:
+<br>
+<iframe src="https://www.youtube.com/embed/LU1zceBNT0g?wmode=opaque&amp;theme=dark"></iframe>
+<br>
+
+Aby zarezerwować miejsce na kursie, konieczna jest wpłata opłaty rezerwacyjnej.
+<br><br>
 Dane do przelewu:
 Krzysztof Stanio Dyktek
 <b>Numer konta:</b> 52 1140 2004 0000 3102 7515 5010
 <b>Tytuł wpłaty:</b> Szkolenie PHP 09
 <b>Kwota:</b> 100 zł
+<br>
 
 W razie pytań pozostajemy do Twojej dyspozycji pod numerem telefonu +48 535 001 087. Możesz też do nas napisać:
 <a href="mailto: kontakt@krzysztof-stanio.pl">kontakt@krzysztof-stanio.pl</a>.
+<br>
 Pozdrawiamy, zespół Od zera do WebDeva
 
