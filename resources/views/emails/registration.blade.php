b220281d3e674312e36253a30b56b63855ef185a
Katarzyna Fedevych <katarzynan@gmail.com>
2017-04-24 15:29:21 +0200

    course registration email view7

--- resources/views/emails/registration.blade.php
+++ resources/views/emails/registration.blade.php
@@ -5,38 +5,10 @@
     <title>Od Zera do WebDeva - zgłoszenie na kurs</title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans|Oswald" rel="stylesheet">
 </head>
-<style>
-    body {
-        background-color: #f2f2f2;
-    }
-
-    h2 {
-        text-align: center;
-        font-family: 'Oswald', sans-serif;
-        color: #555a78;
-        font-weight: bold;
-    }
-
-    img {
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-        width:100%;
-    }
-
-    .email {
-        font-family: 'Open Sans', sans-serif;
-        color: #8f8f8f;
-        text-align: justify;
-    }
-
-
-</style>
-
-<body>
-<img class="logo" src="https://scontent-waw1-1.xx.fbcdn.net/v/t31.0-8/12068870_1503802496610316_5806512324136392188_o.jpg?oh=37d16246f9d63bce91e6dfc30c4152ef&oe=5980965F"></p>
-<h2>Dzień dobry!</h2>
-<div class="email">
+<body style="background-color: #f2f2f2;">
+<img src="https://scontent-waw1-1.xx.fbcdn.net/v/t31.0-8/12068870_1503802496610316_5806512324136392188_o.jpg?oh=37d16246f9d63bce91e6dfc30c4152ef&oe=5980965F" style="display: block;margin-left: auto;margin-right: auto;width: 100%;">
+<h2 style="text-align: center;font-family: 'Oswald', sans-serif;color: #555a78;font-weight: bold;">Dzień dobry!</h2>
+<div class="emailbody" style="font-family: 'Open Sans', sans-serif;color: #8f8f8f;text-align: justify;">
     Dziękujemy za zainteresowanie naszym kursem.<br>
     Najbliższa edycja zacznie się <b>26 maja 2017 r.</b> i będzie trwała 6 tygodni.<br><br>
     Podczas 157-godzinnego kursu zdobędziesz solidne podstawy, które pomogą Ci rozwijać karierę webdevelopera.
