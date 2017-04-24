867a353fdd2de9262e32f063fcdb83f06930ee74
Katarzyna Fedevych <katarzynan@gmail.com>
2017-04-24 15:02:09 +0200

    course registration email view2

--- resources/views/emails/registration.blade.php
+++ resources/views/emails/registration.blade.php
@@ -3,37 +3,77 @@
 <head>
     <meta charset="UTF-8">
     <title>Od Zera do WebDeva - zgłoszenie na kurs</title>
+
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Oswald" rel="stylesheet">
 </head>
-<body bgcolor="#f2f2f2">
-<img class="logo" src="http://krzysztof-stanio.pl/img/logo.png" align="middle"></p>
+<style>
+    body {
+        background-color: #f2f2f2;
+    }
+
+    h2 {
+        text-align: center;
+        font-family: 'Oswald', sans-serif;
+        color: #555a78;
+        font-weight: bold;
+    }
+
+    img {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width:100%
+    }
+
+    .mail {
+        font-family: 'Open Sans', sans-serif;
+        color: #8f8f8f;
+        text-align: justify;
+    }
+
+
+</style>
+
+<body>
+<img class="logo" src="https://scontent-waw1-1.xx.fbcdn.net/v/t31.0-8/12068870_1503802496610316_5806512324136392188_o.jpg?oh=37d16246f9d63bce91e6dfc30c4152ef&oe=5980965F"></p>
 <h2>Dzień dobry!</h2>
-Dziękujemy za zainteresowanie naszym kursem.<br>
-Najbliższa edycja zacznie się <b>26 maja 2017 r.</b> i będzie trwała 6 tygodni.<br><br>
-Podczas 157-godzinnego kursu zdobędziesz solidne podstawy, które pomogą Ci rozwijać karierę webdevelopera.
-Musisz jednak liczyć się z tym, że kurs to nie wszystko - czeka Cię dużo samodzielnej pracy, a kariera programisty
-to nauka, która się nie kończy :-)<br><br>
+<div class="mail">
+    Dziękujemy za zainteresowanie naszym kursem.<br>
+    Najbliższa edycja zacznie się <b>26 maja 2017 r.</b> i będzie trwała 6 tygodni.<br><br>
+    Podczas 157-godzinnego kursu zdobędziesz solidne podstawy, które pomogą Ci rozwijać karierę webdevelopera.
+    Musisz jednak liczyć się z tym, że kurs to nie wszystko - czeka Cię dużo samodzielnej pracy, a kariera programisty
+    to nauka, która się nie kończy :-)<br><br>
 
-Zajęcia prowadzone są w formie bootcampa - wraz z innymi uczestnikami przez 6 tygodni będziesz wykonywał różne zadania, zgodnie z programem
-zamieszczonym na <a href="http://krzysztof-stanio.pl/kursy-programowania" target="_blank">tej stronie</a>.
-Konieczne będzie przyniesienie własnego laptopa, który skonfigurujemy razem, na indywidualnym spotkaniu przed kursem.
+    Zajęcia prowadzone są w formie bootcampa - wraz z innymi uczestnikami przez 6 tygodni będziesz wykonywał różne zadania, zgodnie z programem
+    zamieszczonym na <a href="http://krzysztof-stanio.pl/kursy-programowania" target="_blank">tej stronie</a>.
+    Konieczne będzie przyniesienie własnego laptopa, który skonfigurujemy razem, na indywidualnym spotkaniu przed kursem.
 
-Wszystkie zajęcia prowadzone będą w Krakowie w naszym biurze przy ul. Ketlinga 1.
-W pobliżu można zaparkować samochodem, możliwy jest też dojazd komunikacją miejską liniami: 151, 178, 451. Budynek to domek zaadaptowany na biuro - przestronny i klimatyzowany. Do dyspozycji uczestników pozostaje wyposażona kuchnia i kilka łazienek.
-Osoby które są z daleka lub nie chcą tracić czasu na dojazd mogą bezpłatne nocować na górnych piętrach budynku, specjalnie do tego dostosowanych.
+    Wszystkie zajęcia prowadzone będą w Krakowie w naszym biurze przy ul. Ketlinga 1.
+    W pobliżu można zaparkować samochodem, możliwy jest też dojazd komunikacją miejską liniami: 151, 178, 451. Budynek to dom zaadaptowany na biuro - przestronny i klimatyzowany. Do dyspozycji uczestników pozostaje wyposażona kuchnia i kilka łazienek.
+    Osoby które są z daleka lub nie chcą tracić czasu na dojazd mogą bezpłatne nocować na górnych piętrach budynku, specjalnie do tego dostosowanych.
 
-Aby zarezerwować miejsce na kursie, konieczna jest wpłata opłaty rezerwacyjnej.
-<br><br>
-Dane do przelewu:<br>
-<b>Odbiorca:</b> Krzysztof Stanio Dyktek<br>
-<b>Numer konta:</b> 52 1140 2004 0000 3102 7515 5010<br>
-<b>Tytuł wpłaty:</b> Szkolenie PHP 09<br>
-<b>Kwota:</b> 100 zł<br>
-<br><br>
+    Aby zarezerwować miejsce na kursie, konieczna jest wpłata opłaty rezerwacyjnej.
+    <br><br>
+    Dane do przelewu:<br>
+    <b>Odbiorca:</b> Krzysztof Stanio Dyktek<br>
+    <b>Numer konta:</b> 52 1140 2004 0000 3102 7515 5010<br>
+    <b>Tytuł wpłaty:</b> Szkolenie PHP 09<br>
+    <b>Kwota:</b> 100 zł<br>
+    <br><br>
 
-W razie pytań pozostajemy do Twojej dyspozycji pod numerem telefonu +48 535 001 087.<br>
-Możesz też do nas napisać:
-<a href="mailto: kontakt@krzysztof-stanio.pl">kontakt@krzysztof-stanio.pl</a>.
-<br><br>
-Pozdrawiamy, zespół Od zera do WebDeva
+    Dane organizatora kursu:<br>
+    <b>Krzysztof Stanio Dyktek</b> <br>
+    <b>NIP: </b>8133197081<br>
+    <b>Adres: </b>ul. Ketlinga 1, 32-020 Kraków<br>
+    <br><br>
+
+
+    W razie pytań pozostajemy do Twojej dyspozycji pod numerem telefonu <b>+48 535 001 087</b>.<br>
+    Możesz też do nas napisać:
+    <a href="mailto: kontakt@krzysztof-stanio.pl">kontakt@krzysztof-stanio.pl</a>.
+    <br><br>
+    Pozdrawiamy, zespół Od zera do WebDeva
+</div>
 </body>
 </html>
